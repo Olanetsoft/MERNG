cd241edcb6f4e37c19c3ae3a2baf30730405541a
olanetsoft <heedris2olubisi@gmail.com>
2020-10-27 00:08:16 +0100

    Code restructured for a good develoer experience

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,15 @@
+const Post = require('../../models/Post');
+
+module.exports = {
+    Query: {
+        async getPosts() {
+            try {
+                const posts = await Post.find()
+                return posts;
+            }
+            catch (err) {
+                throw new Error(err)
+            }
+        }
+    }
+}
