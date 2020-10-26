0f7437dcbe66cee245e9135bb32dd4929c4615d4
olanetsoft <heedris2olubisi@gmail.com>
2020-10-26 23:16:53 +0100

    Fixed the query undefined bug on first try

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 
 const typeDefs = gql`
-    type query {
+    type Query {
         sayHi: String!
     }
 `;
