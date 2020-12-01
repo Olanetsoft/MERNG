5f9a0439979f3de6523c17a156eed254ba06c242
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-01 14:54:15 +0100

    Feat: Create post completed

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth')
 
 module.exports = {
     Query: {
@@ -10,6 +11,31 @@ module.exports = {
             catch (err) {
                 throw new Error(err)
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                }
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+           return await newPost.save();
         }
     }
 }
