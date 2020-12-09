624542e271944057fa19bbfc2cd83538894fb862
olanetsoft <heedris2olubisi@gmail.com>
2020-12-09 15:52:38 +0100

    Feat: Added required Comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,13 +1,16 @@
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth')
 
 module.exports = {
     Mutation: {
+
+        // create comment
         createComment: async (_, { postId, body }, context) => {
             const { username } = checkAuth(context);
 
+            // Check if comment body is not empty
             if (body.trim() === '') {
                 throw new UserInputError('Empty Comment', {
                     errors: {
@@ -16,8 +19,10 @@ module.exports = {
                 })
             }
 
+            // find the post by ID
             const post = await Post.findById(postId);
 
+            // If post exist
             if (post) {
                 post.comments.unshift({
                     body,
@@ -30,6 +35,28 @@ module.exports = {
             } else {
                 throw new UserInputError('Post not Found')
             }
+        },
+
+        // Delete comment
+        deleteComment: async (_, { postId, commentId }, context) => {
+            const { username } = checkAuth(context);
+
+            // Fnd post by ID
+            const post = await Post.findById(postId);
+
+           // If post Exist
+            if (post) {
+                const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+                if (post.comments[commentIndex].username === username) {
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            }
+
         }
     }
 }
