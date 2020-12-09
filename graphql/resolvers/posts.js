527302f91314ef9bd93be0b1331c1fc2ae22c7b5
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-09 16:15:40 +0100

    Feat: Like / Unlike Action  completed

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 
 const Post = require('../../models/Post');
@@ -55,6 +55,26 @@ module.exports = {
                 }
             } catch (err) {
                 throw new Error(err);
+            }
+        },
+        async likePost(_, { postId }, context) {
+            const { username } = checkAuth(context);
+            const post = await Post.findById(postId);
+            if (post) {
+                if (post.likes.find(like => like.username === username)) {
+                    // Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+                await post.save();
+                return post;
+            } else {
+                throw new UserInputError('Post not Found');
             }
         }
     }
