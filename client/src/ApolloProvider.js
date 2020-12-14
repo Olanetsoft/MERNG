5dffb620f449580c714b14526a6a383a800601ff
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-14 14:54:07 +0100

    FEAT: Installed several apollo dependencies on client

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,5 @@
+import App from './App'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
