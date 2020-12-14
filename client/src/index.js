ae8fb15768e6892033c2ed415c95640a004867cc
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-14 15:19:56 +0100

    FEAT: Connected server with client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloProvider from './ApolloProvider';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(ApolloProvider,
   document.getElementById('root')
 );
 
