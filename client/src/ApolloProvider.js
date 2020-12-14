f049217abf7f14985ed7c1d239654869b69b4ee1
olanetsoft <heedris2olubisi@gmail.com>
2020-12-14 15:19:56 +0100

    FEAT: Connected server with client

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -3,3 +3,20 @@ import ApolloClient from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks'
+
+// create the httpLink client
+const httpLink = createHttpLink({
+    uri: 'http://localhost:5000'
+});
+
+// create a new instance of the client
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+export default (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+)
