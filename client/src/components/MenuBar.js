72595ba6b0cc0964120b5f82255f497a01906e3a
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-17 16:50:04 +0100

    FIX: implemented active path

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -3,12 +3,17 @@ import { Menu, Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 function MenuBar() {
-    const [activeItem, setActiveItem] = useState('home');//adding home into the useState set the ui active default to home
+
+    // Get the path name and set active as required
+    const pathname = window.location.pathname;
+    const path = pathname === '/' ? 'home' : pathname.substr(1);
+    const [activeItem, setActiveItem] = useState(path);
+
 
     const handleItemClick = (e, { name }) => setActiveItem(name);
 
     return (
-        <Menu pointing secondary>
+        <Menu pointing secondary size="massive" color="blue">
             <Menu.Item
                 name='home'
                 active={activeItem === 'home'}
