8a7f5592b85d79af56a46a5d3ea57b118db53b8b
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-17 16:59:39 +0100

    FIX: implemented active path

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import { useState } from 'react'
+import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 function MenuBar() {
