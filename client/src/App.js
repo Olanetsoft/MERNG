44303590161113374ecb9ae4898428a9464c8191
olanetsoft <heedris2olubisi@gmail.com>
2020-12-17 16:06:42 +0100

    FEAT: installed packages for UI and several pages were created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+// Import pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>Hi World</h1>
-    </div>
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
   );
 }
 
---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Login() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
+
+
+export default Login;
