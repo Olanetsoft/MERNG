e4319b90598aa0f69996b26fa42da37d6352af7d
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-18 13:45:22 +0100

    FEAT: setting up cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,51 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql'
+import gql from 'graphql-tag'
+
+import { Grid } from 'semantic-ui-react'
+import PostCard from '../components/PostCard'
+
 
 function Home() {
+    const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS_QUERY);
+
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <Grid columns={3} divided>
+            <Grid.Row>
+                Recent Posts
+              </Grid.Row>
+            <Grid.Row>
+                {loading ? (
+                    <h1>Posts Loading...</h1>
+                ) : (
+                        posts && posts.map(post => (
+                            <Grid.Column key={post.id}>
+                                <PostCard post={post} />
+                            </Grid.Column>
+                        ))
+                    )}
+            </Grid.Row>
+        </Grid>
     )
 }
 
 const FETCH_POSTS_QUERY = gql`
-    getPosts{
-        id body username createdAt likeCount commentCount
-        like{
-            username
-        }
-        comments{
-            id body createdAt
-        }
+   {
+    getPosts {
+        id
+        body
+        comments {
+        id
+        body
     }
+    commentCount
+    likes {
+      id
+      username
+    }
+    likeCount
+  }
+}
 `
 
 export default Home;
