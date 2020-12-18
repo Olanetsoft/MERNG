29a514d5328093b073903fb159964fe4c0d6f46b
olanetsoft <heedris2olubisi@gmail.com>
2020-12-18 23:42:21 +0100

    FIX: title positioning

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ function Home() {
     return (
 
         <Grid columns={3}>
-            <Grid.Row>
+            <Grid.Row className="page-title">
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
