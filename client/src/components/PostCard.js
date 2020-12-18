a47e48af88e8398e662332fecf6532769796d886
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-18 23:18:08 +0100

    FIX: user details population

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -2,15 +2,16 @@ import { Card, Icon, Label, Image } from 'semantic-ui-react';
 
 import moment from 'moment';
 
-function PostCard({ props: { id, body, createdAt, commentCount, likes, likeCount, username } = {} }) {
+function PostCard({ post: { id, body, createdAt, commentCount, likes, likeCount, username } }) {
 
+    console.log(id);
     return (
         <Card>
             <Card.Content>
                 <Image
                     floated='right'
                     size='mini'
-                    src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+                    src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
                 />
                 <Card.Header>{username}</Card.Header>
                 <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
