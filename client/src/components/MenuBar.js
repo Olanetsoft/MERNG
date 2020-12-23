fc5a39e471f93fb5f01a5b6dd6ac7887ce0a2ab9
asiwajudev <olanrewajuranda@gmail.com>
2020-12-23 23:48:24 +0100

    Preformeted

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,72 +1,58 @@
-import { useContext, useState } from 'react'
-import { Menu } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { useContext, useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-
-import { AuthContext } from '../context/auth'
+import { AuthContext } from "../context/auth";
 
 function MenuBar() {
+  //Destructure user and logout from context
+  const { user, logout } = useContext(AuthContext);
 
-    //Destructure user and logout from context
-    const { user, logout } = useContext(AuthContext);
+  // Get the path name and set active as required
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
-    // Get the path name and set active as required
-    const pathname = window.location.pathname;
-    const path = pathname === '/' ? 'home' : pathname.substr(1);
-    const [activeItem, setActiveItem] = useState(path);
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="blue">
+      <p>Hi 🙌</p>
+      <Menu.Item name={user.username} active as={Link} to="/" />
 
-    const handleItemClick = (e, { name }) => setActiveItem(name);
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
+    <Menu pointing secondary size="massive" color="blue">
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
 
-
-    const menuBar = user ? (
-        <Menu pointing secondary size="massive" color="blue">
-            <Menu.Item
-                name={user.username}
-                active
-                as={Link}
-                to='/'
-            />
-
-            <Menu.Menu position='right'>
-                <Menu.Item
-                    name='logout'
-                    onClick={logout}
-                />
-            </Menu.Menu>
-        </Menu>
-
-    ) : (
-            <Menu pointing secondary size="massive" color="blue">
-                <Menu.Item
-                    name='home'
-                    active={activeItem === 'home'
-                    }
-                    onClick={handleItemClick}
-                    as={Link}
-                    to='/'
-                />
-
-                <Menu.Menu position='right'>
-                    <Menu.Item
-                        name='login'
-                        active={activeItem === 'login'}
-                        onClick={handleItemClick}
-                        as={Link}
-                        to='/login'
-                    />
-                    <Menu.Item
-                        name='register'
-                        active={activeItem === 'register'}
-                        onClick={handleItemClick}
-                        as={Link}
-                        to='/register'
-                    />
-                </Menu.Menu>
-            </Menu >
-
-        )
-    return menuBar;
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === "login"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name="register"
+          active={activeItem === "register"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+  return menuBar;
 }
 
 export default MenuBar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,48 +1,46 @@
-import React, { useContext } from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { Grid, Transition } from 'semantic-ui-react'
+import React, { useContext } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { Grid, Transition } from "semantic-ui-react";
 
-
-import { AuthContext } from '../context/auth'
-import PostCard from '../components/PostCard'
-import PostForm from '../components/PostForm'
-import { FETCH_POSTS_QUERY } from '../utils/graphql'
-
+import { AuthContext } from "../context/auth";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../utils/graphql";
 
 function Home() {
-    // get user
-    const { user } = useContext(AuthContext);
+  // get user
+  const { user } = useContext(AuthContext);
 
-    const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+  const { loading, data: { getPosts: posts } = {} } = useQuery(
+    FETCH_POSTS_QUERY
+  );
 
-    return (
-
-        <Grid columns={3}>
-            <Grid.Row className="page-title">
-                <h1>Recent Posts</h1>
-            </Grid.Row>
-            <Grid.Row>
-                {user && (
-                    <Grid.Column>
-                        <PostForm />
-                    </Grid.Column>
-                )}
-                {loading ? (
-                    <h1>Posts Loading...</h1>
-                ) : (
-                        <Transition.Group>
-                            {posts && posts.map((post) => (
-                                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                                    <PostCard post={post} />
-                                </Grid.Column>
-                            ))}
-                        </Transition.Group>
-                    )}
-            </Grid.Row>
-
-        </Grid>
-
-    )
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
+        {loading ? (
+          <h1>Posts Loading...</h1>
+        ) : (
+          <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 }
 
 // const FETCH_POSTS_QUERY = gql`
