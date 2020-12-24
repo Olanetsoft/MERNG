a3e8bba6a9e4c88e1af2a19adc9e8b241f584584
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-24 03:37:07 +0100

    CHECK

--- util/validators.js
+++ util/validators.js
@@ -1,45 +1,45 @@
 module.exports.validateRegisterInput = (
-    username,
-    email,
-    password,
-    confirmPassword
+  username,
+  email,
+  password,
+  confirmPassword
 ) => {
-    const errors = {};
+  const errors = {};
 
-    if (username.trim() === '') {
-        errors.username = 'Username cannot be empty'
+  if (username.trim() === "") {
+    errors.username = "Username cannot be empty";
+  }
+  if (email.trim() === "") {
+    errors.email = "Email cannot be empty";
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Not a Valid Email";
     }
-    if (email.trim() === '') {
-        errors.email = 'Email cannot be empty'
-    } else {
-        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-        if (!email.match(regEx)) {
-            errors.email = 'Not a Valid Email';
-        }
-    }
-    if (password === '') {
-        errors.password = 'Password cannot be empty'
-    } else if (password !== confirmPassword) {
-        errors.confirmPassword = 'Password must match'
-    }
+  }
+  if (password === "") {
+    errors.password = "Password cannot be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Password must match";
+  }
 
-    return {
-        errors,
-        valid: Object.keys(errors).length < 1
-    }
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
 };
 
 module.exports.validateLoginInput = (username, password) => {
-    const errors = {};
+  const errors = {};
 
-    if (username.trim() === '') {
-        errors.username = 'Username cannot be empty'
-    }
-    if (password === '') {
-        errors.password = 'Password cannot be empty'
-    }
-    return {
-        errors,
-        valid: Object.keys(errors).length < 1
-    }
+  if (username.trim() === "") {
+    errors.username = "Username cannot be empty";
+  }
+  if (password === "") {
+    errors.password = "Password cannot be empty";
+  }
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
 };
