bb55b57f8c8cc4227732baffe26a79c168ce8b6c
asiwajudev <olanrewajuranda@gmail.com>
2020-12-24 02:15:31 +0100

    FEAT: Add comment  completed

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,64 +1,61 @@
-const { UserInputError, AuthenticationError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require("apollo-server");
 
-const Post = require('../../models/Post');
-const checkAuth = require('../../util/check-auth')
+const Post = require("../../models/Post");
+const checkAuth = require("../../util/check-auth");
 
 module.exports = {
-    Mutation: {
+  Mutation: {
+    // create comment
+    createComment: async (_, { postId, body }, context) => {
+      const { username } = checkAuth(context);
 
-        // create comment
-        createComment: async (_, { postId, body }, context) => {
-            const { username } = checkAuth(context);
+      // Check if comment body is not empty
+      if (body.trim() === "") {
+        throw new UserInputError("Empty Comment", {
+          errors: {
+            body: "Comment body cannot be empty",
+          },
+        });
+      }
 
-            // Check if comment body is not empty
-            if (body.trim() === '') {
-                throw new UserInputError('Empty Comment', {
-                    errors: {
-                        body: 'Comment body cannot be empty'
-                    }
-                })
-            }
+      // find the post by ID
+      const post = await Post.findById(postId);
 
-            // find the post by ID
-            const post = await Post.findById(postId);
+      // If post exist
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString(),
+        });
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not Found");
+      }
+    },
 
-            // If post exist
-            if (post) {
-                post.comments.unshift({
-                    body,
-                    username,
-                    createdAt: new Date().toISOString()
+    // Delete comment
+    deleteComment: async (_, { postId, commentId }, context) => {
+      const { username } = checkAuth(context);
 
-                })
-                await post.save();
-                return post;
-            } else {
-                throw new UserInputError('Post not Found')
-            }
-        },
+      // Fnd post by ID
+      const post = await Post.findById(postId);
 
-        // Delete comment
-        deleteComment: async (_, { postId, commentId }, context) => {
-            const { username } = checkAuth(context);
+      // If post Exist
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
 
-            // Fnd post by ID
-            const post = await Post.findById(postId);
-
-            // If post Exist
-            if (post) {
-                const commentIndex = post.comments.findIndex(c => c.id === commentId);
-
-                if (post.comments[commentIndex].username === username) {
-                    post.comments.splice(commentIndex, 1);
-                    await post.save();
-                    return post;
-                } else {
-                    throw new AuthenticationError('Action not allowed')
-                }
-            } else {
-                throw new UserInputError('Post not Found')
-            }
-
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
         }
-    }
-}
+      } else {
+        throw new UserInputError("Post not Found");
+      }
+    },
+  },
+};
