d2fe8c0c8d85d8473ad01563d5910c26a8449958
AsiwajuDev <olanrewajuranda@gmail.com>
2020-12-24 03:39:42 +0100

    Tryin to fix commit account

--- util/check-auth.js
+++ util/check-auth.js
@@ -1,25 +1,23 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError } = require("apollo-server");
 
-const jwt = require('jsonwebtoken');
-const { SECRET_KEY } = require('../config');
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
 
 module.exports = (context) => {
-
-    // check auth header
-    const authHeader = context.req.headers.authorization;
-    if (authHeader) {
-        // Bearer ...
-        const token = authHeader.split('Bearer ')[1];
-        if (token) {
-            try {
-                const user = jwt.verify(token, process.env.SECRET_KEY || SECRET_KEY);
-                return user;
-            } catch (err) {
-                throw new AuthenticationError('Invalid / Expired token')
-            }
-        }
-        throw new Error("Authentication token must be \'Bearer [token]")
+  // check auth header
+  const authHeader = context.req.headers.authorization;
+  if (authHeader) {
+    // Bearer ...
+    const token = authHeader.split("Bearer ")[1];
+    if (token) {
+      try {
+        const user = jwt.verify(token, process.env.SECRET_KEY || SECRET_KEY);
+        return user;
+      } catch (err) {
+        throw new AuthenticationError("Invalid / Expired token");
+      }
     }
-    throw new Error('Authorization header must be provided')
-
-}
+    throw new Error("Authentication token must be 'Bearer [token]");
+  }
+  throw new Error("Authorization header must be provided");
+};
